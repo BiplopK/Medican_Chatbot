7f58feefc4a821b713fce6aa4b3fd8b8e3ff1041
BiplopK <khadkabiplop10@gmail.com>
2026-03-01 10:24:41 +0545

    Initial Commit

---
+++ template.sh
@@ -0,0 +1,14 @@
+
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompts.py
+touch .env
+touch app.py
+touch setup.py
+touch requirements.txt
+touch research/trails.py
+
+echo "Directory and Files created Succesfully."
